d7b0e70ff59fe06aaa6b3f6e71fdf075b2ba86f9
ManjulSinghSachan <sachanmanjul@gmail.com>
2017-05-08 23:25:43 +0530

    Product Details

---
+++ app/src/main/java/snu/spm/unisell/activity/ProductDetailsActivity.java
@@ -0,0 +1,15 @@
+package snu.spm.unisell.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import snu.spm.unisell.R;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+    }
+}
