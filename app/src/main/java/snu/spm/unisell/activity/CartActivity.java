200044423a4d5590a248ae11485582d0eb31a9bb
ManuMaheshwari <maheshwarimanu11@gmail.com>
2017-05-08 22:47:12 +0530

    missing files

---
+++ app/src/main/java/snu/spm/unisell/activity/CartActivity.java
@@ -0,0 +1,10 @@
+package snu.spm.unisell.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ManuMaheshwari on 5/8/17.
+ */
+
+public class CartActivity extends AppCompatActivity {
+}
